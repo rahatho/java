dbe68da054f961e99a6b00c04256d52cde9b33e6
rahatho <rahat.ho@gmail.com>
2019-09-13 22:36:19 +0600

    Add Test Class main

--- src/SimpleEx.java
+++ src/SimpleEx.java
@@ -1,2 +1,17 @@
-public class SimpleEx {
+import javax.swing.*;
+
+public class SimpleEx  extends JFrame {
+
+    public SimpleEx(){
+        initUI();
+    }
+
+    private void initUI() {
+        setTitle("Simple ex");
+        setSize(300,300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
 }
+
