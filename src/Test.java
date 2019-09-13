6a483c03d8ac6c1eff389134dc690c54181931a0
rahatho <rahat.ho@gmail.com>
2019-09-13 22:40:07 +0600

    test simpleEx class

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,12 @@
+import java.awt.*;
+
 public class Test {
+
+    public static void main(String[] args){
+        EventQueue.invokeLater(()->{
+
+            var ex = new SimpleEx();
+            ex.setVisible(true);
+        });
+    }
 }
